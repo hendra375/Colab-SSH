2e3001a9e51044a773c1b5842a7c420606bdca3f
hendra375 <30514707+hendra375@users.noreply.github.com>
2022-02-18 16:36:01 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/hendra375/Colab-SSH/main/gcngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
